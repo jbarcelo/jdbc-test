e112e499675930fee50ba7584f12a3b667c2ae1d
Jaume Barcelo <jaume.barcelo@gmail.com>
2023-03-24 17:19:39 +0100

    updates to part2, CRUD

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,8 +1,6 @@
 package com.example;
  
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
  
 public class Application {
  
@@ -11,6 +9,16 @@ public class Application {
     public static void main(String[] args) throws SQLException {
         try {
             openDatabaseConnection();
+            deleteData("%");
+            readData();
+            createData("Java", 10);
+            createData("JavaScript", 9);
+            createData("C++", 8);
+            readData();
+            updateData("C++", 7);
+            readData();
+            deleteData("C++");
+            readData();
         } finally {
             closeDatabaseConnection();
         }
@@ -32,4 +40,62 @@ public class Application {
         System.out.println("Connection valid: " + connection.isValid(0));
     }
  
+    private static void createData(String name, int rating) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("""
+                    INSERT INTO programming_language(name, rating)
+                    VALUES (?, ?)
+                """)) {
+            statement.setString(1, name);
+            statement.setInt(2, rating);
+            int rowsInserted = statement.executeUpdate();
+            System.out.println("Rows inserted: " + rowsInserted);
+        }
+    }
+
+    private static void readData() throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("""
+                    SELECT name, rating
+                    FROM programming_language
+                    ORDER BY rating DESC
+                """)) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                boolean empty = true;
+                while (resultSet.next()) {
+                    empty = false;
+                    String name = resultSet.getString("name");
+                    int rating = resultSet.getInt("rating");
+                    System.out.println("\t> " + name + ": " + rating);
+                }
+                if (empty) {
+                    System.out.println("\t (no data)");
+                }
+            }
+        }
+    }
+
+    private static void updateData(String name, int newRating) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("""
+                    UPDATE programming_language
+                    SET rating = ?
+                    WHERE name = ?
+                """)) {
+            statement.setInt(1, newRating);
+            statement.setString(2, name);
+            int rowsUpdated = statement.executeUpdate();
+            System.out.println("Rows updated: " + rowsUpdated);
+        }
+    }
+
+    private static void deleteData(String nameExpression) throws SQLException {
+       try (PreparedStatement statement = connection.prepareStatement("""
+                   DELETE FROM programming_language
+                   WHERE name LIKE ?
+               """)) {
+           statement.setString(1, nameExpression);
+           int rowsDeleted = statement.executeUpdate();
+           System.out.println("Rows deleted: " + rowsDeleted);
+       }
+    }
+
 }
+
